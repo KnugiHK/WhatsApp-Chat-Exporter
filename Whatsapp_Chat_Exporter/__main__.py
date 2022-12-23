4b357d5ea9ddc4aa4516018face182f0f93cb1c6
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2022-12-23 16:49:28 +0800

    Update the import of Crypt to latest one for message

--- Whatsapp_Chat_Exporter/__main__.py
+++ Whatsapp_Chat_Exporter/__main__.py
@@ -1,7 +1,7 @@
 from .__init__ import __version__
 from Whatsapp_Chat_Exporter import extract, extract_iphone
 from Whatsapp_Chat_Exporter import extract_iphone_media
-from Whatsapp_Chat_Exporter.extract import Crypt
+from Whatsapp_Chat_Exporter.extract_new import Crypt
 from optparse import OptionParser
 import os
 import sqlite3
