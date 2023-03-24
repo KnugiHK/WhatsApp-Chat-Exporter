8f0511a6e22885d26da6e5cdf7f505ab34ee244f
GoComputing <carlosprog1997@gmail.com>
2023-03-24 19:43:27 +0100

    Re-enabled the HTML generation

--- Whatsapp_Chat_Exporter/__main__.py
+++ Whatsapp_Chat_Exporter/__main__.py
@@ -184,7 +184,7 @@ def main():
             messages(db, data)
             media(db, data, options.media)
             vcard(db, data)
-        # create_html(data, options.output, options.template, options.embedded)
+        create_html(data, options.output, options.template, options.embedded)
     else:
         print(
             "The message database does not exist. You may specify the path "
