ff95625edff77c0d93cac3e9bb30645f120f23a0
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2023-06-16 17:50:08 +0800

    Dropping Python 3.7 and adding Python 3.11 support

--- setup.py
+++ setup.py
@@ -31,10 +31,10 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
